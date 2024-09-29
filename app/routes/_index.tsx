b4c5dc6162c37fc059e42329ffe2b95a4fa291ce
Serphis <serphis48@wp.pl>
2024-09-29 14:54:52 +0200

    returned modal

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -32,7 +32,7 @@ export default function Index() {
     <Layout>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <div className="bg-slate-100">
-        {/* <Modal isOpen={isModalOpen} onClose={closeModal} /> */}
+        <Modal isOpen={isModalOpen} onClose={closeModal} />
 
         <div id="about" className="w-full md:py-4">
           <div
